12716cb5a8a3fe78cc1b08de743a652b8a37009c
nandish145 <nandish.mahadevaiah@gmail.com>
2014-04-10 02:41:28 -0400

    add

---
+++ NextMultiple.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+int nextlargemultiple(int x,int n)
+{
+	int next = n;
+
+	while( next < x)
+		next <<=1;
+
+	while(next > x)
+		next -= n;
+
+	if( next == x)
+		return next;
+
+	return next+n;
+}
+
+int main(int argc, char* argv[])
+{
+	if( argc != 2)
+	{
+		cout<<"File name required as argument"<<endl;
+		return -1;
+	}
+
+	fstream file;
+	file.open(argv[1],ios::in);
+	char line[25];
+
+	while(!file.eof())
+	{
+		file.getline(line,25);
+		string str = string(line);
+		if( str.length() == 0)
+			continue;
+		int space_index = str.find(',');
+		int x = atoi(str.substr(0,space_index).c_str());
+		str = str.substr(space_index+1);
+		int n = atoi(str.c_str());
+		cout<<nextlargemultiple(x,n)<<endl;
+	}
+
+	return 0;
+}
